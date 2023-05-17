ede9919e101f847d565f2383b171284a2b2e025b
am1rfy <fajzov.amir.gl@gmail.com>
2023-05-17 05:51:21 +0500

    mongo attached

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const http = require('http')
 const path = require('path')
+require('dotenv').config()
 
 const router = require('./router')
-const api = require('./api')
+const authRouter = require('./api/auth')
+const feedbackRouter = require('./api/feedback')
 
 const app = express()
 
@@ -15,7 +18,19 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', router)
-app.use('/api', api)
+app.use('/api/auth', authRouter)
+app.use('/api/feedback', feedbackRouter)
 
-const server = http.createServer(app)
-server.listen(process.env.PORT ?? '8080')
+const start = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${ process.env.DB_USERNAME }:${ process.env.DB_PASSWORD }@amd.dqpyoyj.mongodb.net/?retryWrites=true&w=majority`
+        )
+        http.createServer(app).listen(process.env.PORT ?? '8080')
+    } 
+    catch (e) {
+        console.log(e)
+    }
+}
+
+start()
